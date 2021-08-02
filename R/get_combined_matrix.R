42bec0067c3af708f718bdff1262639e24776df9
Enrico Gaffo <enrico.gaffo@gmail.com>
2021-08-02 23:27:35 +0200

    added option to filter the (output) matrix like in CirComPara2

--- R/get_combined_matrix.R
+++ R/get_combined_matrix.R
@@ -2,27 +2,65 @@
 #'
 #' @param x the list of the methods' output to be combined or the path of the
 #'   CirComPara2's results
-#' @param filtfun a function to filter the rows in the combined matrix
+#' @param filtfun a function to filter the rows in the combined matrix. No
+#'   filter is applied by default. Set "ccp2" to apply a CirComPara2-like
+#'   filter, that is keep only circRNAs detected with at least N backspliced
+#'   reads and by at least M methods, where N and M are defined by the
+#'   \code{min_bjr} and \code{min_methods} parameters.
 #' @param name_sep the character(s) used to concatenate SAMPLE and METHOD names
 #' @param as_data_table if TRUE, return a data.table instead of a matrix
+#' @param min_bjr minimum amount of backspliced reads for a circRNA to be kept
+#'   when the ccp2 filter is enabled
+#' @param min_methods minimum number of methods that commonly detected a circRNA
+#'   to be kept. It applies to the ccp2 filter.
+#' @param hard_threshold FALSE by default, meaning that if a circRNA passes the
+#'   filtering in one sample, then its values in other samples are preserved
+#'   even when they are below the specified threshold. For instance, if
+#'   circRNA_x has 2 BJRs according to Method_M1 and 2 BJRs according to
+#'   Method_M2 in Sample_A, then it passes the filter. If, in Sample_B,
+#'   circRNA_x has 1 BJRs according to Method_M1 and 1 BJRs according to
+#'   Method_M2, it would not pass the filter, but the 1 values are kept since
+#'   there is some stronger evidence of expression in another sample. Set TRUE
+#'   to apply an hard threshold, meaning that the 1 values will be set to zeros
+#'   following the reasoning that if there is not enough BJRs, the circRNA is
+#'   not considered as being detected. Below an example of the output matrix
+#'   with:
+#'
+#'   \code{hard_threshold = FALSE} (the default)
+#'
+#'   \tabular{ccccc}{ circ_id \tab S_A.M_M1 \tab S_A.M_M2 \tab S_B.M_M1 \tab
+#'   S_B.M_M2 \cr circRNA_x \tab 2 \tab 2 \tab 1 \tab 1 }
+#'
+#'
+#'   or \code{hard_threshold = TRUE}
+#'
+#'   \tabular{ccccc}{ circ_id \tab S_A.M_M1 \tab S_A.M_M2 \tab S_B.M_M1 \tab
+#'   S_B.M_M2 \cr circRNA_x \tab 2 \tab 2 \tab 0 \tab 0 }
 #'
 #' @return a matrix of the expression values with samples in columns and
 #'   features in the rows. Column names have the form SAMPLE.METHOD.
 #' @export
 #'
 #' @examples
-#' \dontrun{mat <- get_combined_matrix(x = "/home/user/CirComPara2_analysis")}
+#' \dontrun{
+#' mat <- get_combined_matrix(x = "/home/user/CirComPara2_analysis")
+#' }
 #' @import data.table
 get_combined_matrix <-
-  function (x,
+  function(x,
             filtfun = NULL,
             name_sep = ".",
-            as_data_table = F) {
+            as_data_table = F,
+            min_bjr = 2,
+            min_methods = 2,
+            hard_threshold = F) {
 
+    ## read the file
     gtf <-
       fread(file = file.path(x,
-                      "circular_expression", "circrna_collection",
-                      "circrnas.gtf"),
+                             "circular_expression",
+                             "circrna_collection",
+                             "circrnas.gtf"),
             header = F,
             select = c(1, 2, 4, 5, 6, 7,
                        9))[, .(Method = V2,
@@ -31,6 +69,34 @@ get_combined_matrix <-
                                                   "\\1", V9),
                                   circ_id = paste0(V1, ":", V4, "-", V5))]
 
+    ## apply filter (if any)
+    if (!is.null(filtfun)) {
+
+      ## apply the CirComPara2-like filter
+      if (filtfun == "ccp2") {
+
+        ## select circRNAs with enough BJRs in at least one samples
+        ## and detected by enough methods
+        keep <-
+          gtf[BJR >= min_bjr, .N, by = .(sample_id, circ_id)
+              ][N >= min_methods, unique(circ_id)]
+
+          if (hard_threshold) {
+
+            ## filter out circRNAs with not enough BJRs, as if they were
+            ## not detected
+            gtf <- gtf[BJR >= min_bjr]
+
+          }
+
+        gtf <- gtf[circ_id %in% keep]
+
+      } else {
+        message("Only 'ccp2' currently allowed. No filter will be applied")
+      }
+    }
+
+    ## put into wide format
     mat_dt <-
       dcast(data = gtf,
             formula = circ_id ~ sample_id + Method,
@@ -38,9 +104,14 @@ get_combined_matrix <-
             fill = 0,
             value.var = "BJR")
 
-    if(as_data_table) {
+    ## return the matrix
+    if (as_data_table) {
+
       mat_dt
+
     } else {
+
       as.matrix(data.frame(mat_dt, row.names = "circ_id"))
+
     }
   }
